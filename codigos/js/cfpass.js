769583ce9e564e50ebb2c519b0a9b06e32c2ad8b
Markin9100 <mvinicius9100@gmail.com>
2022-05-24 10:03:02 -0300

    FlitzTech 0.15
    Sistema de cadastro linkado e funcionando com o Firebase!

---
+++ codigos/js/cfpass.js
@@ -0,0 +1,14 @@
+
+        var password = document.getElementById("pass")
+    confirm_password = document.getElementById("cfpass");
+
+function validatePassword(){
+  if(password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Senhas diferentes!");
+  } else {
+    confirm_password.setCustomValidity('');
+  }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
